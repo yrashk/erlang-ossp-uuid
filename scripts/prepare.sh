eae97d43d79b6576e25015582f2ee6c7e15ed635
Yurii Rashkovskii <yrashk@gmail.com>
2011-02-21 02:25:12 -0800

    Enforce -fPIC on Linux

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
+LINUX=`uname | grep Linux | wc -l | xargs echo`
+if [ -z "$LINUX" ]; then
+   CFLAGS=
+else
+   CFLAGS=-fPIC
+fi
 
-[ -d deps/uuid-1.6.2 ] || (cd deps && tar xzvfp uuid-1.6.2.tar.gz && cd uuid-1.6.2 && ./configure -disable-debug --without-perl --without-php --without-pgsql && make)
+[ -d deps/uuid-1.6.2 ] || (cd deps && tar xzvfp uuid-1.6.2.tar.gz && cd uuid-1.6.2 && CFLAGS=$CFLAGS ./configure -disable-debug --without-perl --without-php --without-pgsql && make)
