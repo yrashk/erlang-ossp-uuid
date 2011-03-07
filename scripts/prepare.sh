79671c7fab0d10fd1124684df900a8b951fb2053
Yurii Rashkovskii <yrashk@gmail.com>
2011-03-06 16:25:36 -0800

    Build ossp-uuid with -m32 if erlang is 32 bit

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,4 +6,10 @@ else
    CFLAGS=-fPIC
 fi
 
+ERLWS=`erl -noshell -eval "io:format(\"~p\",[erlang:system_info(wordsize)])" -s erlang halt`
+
+if [ "$ERLWS" == 4 ]; then
+   CFLAGS=-m32
+fi
+
 [ -d deps/uuid-1.6.2 ] || (cd deps && tar xzvfp uuid-1.6.2.tar.gz && cd uuid-1.6.2 && CFLAGS=$CFLAGS ./configure -disable-debug --without-perl --without-php --without-pgsql && make)
