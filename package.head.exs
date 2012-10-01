ee7bb6ac02f96abe6505c88b2696dc7aa1921e15
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-01 12:05:33 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "ossp_uuid", description: "NIF-based ossp-uuid binding",
+                 version: :head, keywords: ["Erlang","UUID"],
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 contributors: [[name: "Michael Santos", email: "michael.santos@gmail.com"],
+                                [name: "OJ Reeves", email: "oj@buffered.io"]],
+                 repositories: [[github: "yrashk/erlang-ossp-uuid"]])
